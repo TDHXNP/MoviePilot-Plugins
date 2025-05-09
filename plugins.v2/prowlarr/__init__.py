8151024dbb9328601bf536c5c5ca739556104f1a
TDHX <nianpengxiao@gmail.com>
2025-05-09 18:45:23 +0800

    Bug 修复

--- plugins.v2/prowlarr/__init__.py
+++ plugins.v2/prowlarr/__init__.py
@@ -24,7 +24,7 @@ class Prowlarr(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/Prowlarr/Prowlarr/refs/heads/develop/src/Prowlarr.ico"
     # 插件版本
-    plugin_version = "1.9"
+    plugin_version = "1.10"
     # 插件作者
     plugin_author = "TDHXNP"
     # 作者主页
@@ -267,7 +267,7 @@ class Prowlarr(_PluginBase):
                 "search": {
                     "paths": [
                         {
-                            "path": f"api/v1/indexer/{indexer_id}/newznab?t=search&extended=1&limit=100&apikey=7bbe709363cc489ea8671ee3183741a1&q={{keyword}}&cat={{cat}}",
+                            "path": f"api/v1/indexer/{indexer_id}/newznab?t=search&extended=1&limit=100&apikey=7bbe709363cc489ea8671ee3183741a1&q={{keyword}}",
                             "method": "get"
                         }
                     ]
